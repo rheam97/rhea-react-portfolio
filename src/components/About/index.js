56c99aaf33a986d02c89cf0110e9a04d4b06b501
Rhea <rheam97@gmail.com>
2022-04-17 18:41:06 -0400

    added resume and link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,23 @@ import React from 'react';
 function About(){
     return(
         <section>
-            <h1 id = 'about'>Rhea Malviya | Aspiring Full-Stack Developer</h1>
-            <p> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-            </p>
-            <p>this is it</p>
+           <h1 id="about" className="py-3 text-center heading">
+        Rhea Malviya | Aspiring Full-Stack Developer
+      </h1>
+
+      <div className="text-center bio">
+        <p>
+        I'm a soon-to-be full stack developer with a previous
+                    interdisciplinary background in International Relations, Anthropology, Social and Public Policy, and
+                    applications of Social Psychology
+                    and Neuroscience to policymaking. Learning new things constantly has been and continues to be my
+                    bread-and-butter, which is why I feel suited to the field of software development. I am proficient in RESTful APIs, vanilla JS, and advanced CSS frameworks and am currently
+                    learning building MERN apps and honing my CS fundamentals. 
+          <br />
+          <br /> I look forward to hearing from you
+          regarding joining your team as a driven, personable, and adaptable member.
+        </p>
+      </div>
         </section>
     )
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
-
-function Resume(){
-
+import React from "react";
+import resimg from "../../assets/images/resimg.png";
+import resfile from "../../assets/downloads/Rhea2022Resume.pdf";
+import './index.scss'
+function Resume() {
+  return (
+    <div className="res">
+      <h1> Resume and Tech-Stack </h1>
+      <div className="resume">
+        <img src={resimg} className="resFile" alt="Resume screenshot" />
+        <div className="download">
+        <a href={resfile}>
+          <h2>Click to Download my Resume</h2>
+        </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
